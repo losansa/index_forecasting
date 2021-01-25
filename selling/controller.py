618b5d4d25b4ceb4e652cc1072ac15cd47604f04
Konstantin <na7rix@mail.ru>
2021-01-25 23:50:52 +0300

    add unnecessary filtration

--- selling/controller.py
+++ selling/controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from flask.json import jsonify
 
-from selling import repository, serializer
+from selling import service, serializer
 
 
 selling_routes = Blueprint('hello', __name__)
@@ -9,12 +9,12 @@ selling_routes = Blueprint('hello', __name__)
 
 @selling_routes.route('/quote-name')
 def get_all_quotes_names():
-    all_quote_names = repository.get_all_quotes_names()
+    all_quote_names = service.get_all_quotes_names()
     return jsonify(serializer.convert_quotes_names_to_dict(all_quote_names))
 
 
 @selling_routes.route('/quote')
 def get_all_quotes():
     quote_id = request.args.get('quoteId')
-    filtered_quotes = repository.get_filtered_quotes(quote_id)
+    filtered_quotes = service.get_filtered_quotes(quote_id)
     return jsonify(serializer.convert_quotes_to_dict(filtered_quotes))
