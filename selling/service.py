4b4b831e09c9c68d7ae67b0c0593a382fb1809bb
Konstantin <na7rix@mail.ru>
2021-01-25 23:51:22 +0300

    fix unnecessary filtration

--- selling/service.py
+++ selling/service.py
@@ -9,5 +9,5 @@ def get_filtered_quotes(quote_name_id):
     if quote_name_id:
         quotes = repository.get_filtered_quotes(quote_name_id)
     else:
-        quotes = repository.get_all_quotes(quote_name_id)
+        quotes = repository.get_all_quotes()
     return quotes
