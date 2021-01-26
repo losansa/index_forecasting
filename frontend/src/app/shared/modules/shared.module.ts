34421b5ab93e162a65ebf34121bf07f565d5c41b
Andrey Naumov <andreu.naumoff@gmail.com>
2021-01-26 21:30:53 +0300

    add shared module

---
+++ frontend/src/app/shared/modules/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PrimeNgModule } from 'src/app/shared/modules/prime-ng.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    PrimeNgModule,
+  ],
+  exports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    PrimeNgModule,
+  ],
+})
+export class SharedModule {}
