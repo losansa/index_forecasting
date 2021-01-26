27ef42726521d2077c1bffc80fbe8ada00a73820
Andrey Naumov <andreu.naumoff@gmail.com>
2021-01-26 21:24:42 +0300

    add quote service

---
+++ frontend/src/app/shared/services/quote.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { QuoteInterface } from 'src/app/shared/types/quote.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuoteService {
+  constructor(private http: HttpClient) {}
+
+  fetch(): Observable<QuoteInterface[]> {
+    return this.http.get<QuoteInterface[]>('/api/quote');
+  }
+}
