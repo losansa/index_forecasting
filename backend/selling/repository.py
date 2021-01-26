72ea10c3340ff96cee84cad4550f745d1d88e798
Konstantin <na7rix@mail.ru>
2021-01-26 23:11:29 +0300

    add test lstm model

--- backend/selling/repository.py
+++ backend/selling/repository.py
@@ -16,3 +16,9 @@ def get_filtered_quotes(quote_name_id):
     query = db.session.query(Quote)
     query = query.filter(Quote.q_name_id == quote_name_id)
     return query.all()
+
+
+def get_quote_closes_values():
+    query = db.session.query(Quote.q_close)
+    query = query.filter(Quote.q_name_id == 1)
+    return query.all()
