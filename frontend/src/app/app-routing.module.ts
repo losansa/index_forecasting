321e31ab8e6875cc5343c563357cee804f5371a8
Andrey Naumov <andreu.naumoff@gmail.com>
2021-01-27 22:07:34 +0300

    add main-layout.component.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
+import {MainLayoutComponent} from 'src/app/shared/components/main-layout/main-layout.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'quote',
+        loadChildren: () =>
+          import('./quote/quote.module').then((m) => m.QuoteModule),
+      },
+    ],
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {AppComponent} from './app.component'
 import {AppRoutingModule} from './app-routing.module'
 import {SharedModule} from 'src/app/shared/modules/shared.module'
-import {QuoteModule} from 'src/app/quote/quote.module'
+import {QuoteModule} from 'src/app/quote/quote.module';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainLayoutComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
