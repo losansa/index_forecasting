7a739affab65d35b61ca2cd74f6b0b11d92da076
Andrey Naumov <andreu.naumoff@gmail.com>
2021-01-27 22:06:50 +0300

    add prime-ng

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core'
+import {PrimeNGConfig} from 'primeng/api'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true
+  }
+}
--- frontend/src/app/shared/modules/prime-ng.module.ts
+++ frontend/src/app/shared/modules/prime-ng.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core'
 import {TableModule} from 'primeng/table'
 import {CardModule} from 'primeng/card'
 import {MenubarModule} from 'primeng/menubar'
+import {TabMenuModule} from 'primeng/tabmenu'
 
 @NgModule({
-  imports: [TableModule, CardModule, MenubarModule],
-  exports: [TableModule, CardModule, MenubarModule],
+  imports: [TableModule, CardModule, MenubarModule, TabMenuModule],
+  exports: [TableModule, CardModule, MenubarModule, TabMenuModule],
 })
 export class PrimeNgModule {}
