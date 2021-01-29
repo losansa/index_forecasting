bff35510c02bfdf4ddbbbbb188174c12b5e6d723
Andrey Naumov <andreu.naumoff@gmail.com>
2021-01-29 21:55:47 +0300

    add empty chart component

---
+++ frontend/src/app/quote/components/quote-chart/quote-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-chart',
+  templateUrl: './quote-chart.component.html',
+  styleUrls: ['./quote-chart.component.scss']
+})
+export class QuoteChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
